1d5c2d56f468fe5160fbd1be23026aee831c2f56
James <jamescarclan@yahoo.com>
2020-06-18 23:54:56 +0200

    VikingVersion1

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,24 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(theDamage) {}
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(health, strength, name) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage() {}
+  battleCry() {}
+}
 
 // Saxon
 class Saxon {}
