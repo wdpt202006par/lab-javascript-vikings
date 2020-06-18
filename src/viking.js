f629a7213eefeeae847285e1dc47f259374dd004
Clementine <clementinenocton@hotmail.fr>
2020-06-18 19:57:15 +0200

    Missing iteration 4

--- src/viking.js
+++ src/viking.js
@@ -16,10 +16,71 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+
+  battleCry() {
+    return 'Odin Owns You All!';
+  }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+
+  addViking(Viking) {
+    this.vikingArmy.push(Viking);
+  }
+
+  addSaxon(Saxon) {
+    this.saxonArmy.push(Saxon);
+  }
+
+  vikingAttack() {
+    const randomViking = Math.floor(Math.random * this.vikingArmy.length);
+    const randomSaxon = Math.floor(Math.random * this.saxonArmy.length);
+
+    randomSaxon.receiveDamage(randomViking.health);
+  }
+
+  saxonAttack() {
+    const randomViking = Math.floor(Math.random * this.vikingArmy.length);
+    const randomSaxon = Math.floor(Math.random * this.saxonArmy.length);
+
+    randomViking.receiveDamage(randomSaxon.health);
+  }
+
+  showStatus() {
+
+  }
+
+}
