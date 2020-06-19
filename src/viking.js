9fcd623d5b18b189088ed64a77017be30f297440
CassandreV <cassandre.vanzetta@live.fr>
2020-06-19 20:30:16 +0200

    SUPER BONUS done - exercise fully done

--- src/viking.js
+++ src/viking.js
@@ -62,33 +62,60 @@ class War {
         this.saxonArmy.push(Saxon);
     }
 
-    vikingAttack() {
-        var randomSaxon = Math.floor(Math.random()* this.saxonArmy.length);
-        var randomViking = Math.floor(Math.random()*this.vikingArmy.length);
-        let attackResult = this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength);
-        
-        // Keep alive Saxons only
-        this.saxonArmy = this.saxonArmy.filter(function (aliveSaxons) {
-            return aliveSaxons.health > 0;
-        });
+    // ITERATION 4 VERSION
 
-        return attackResult;
-    }
-
-    saxonAttack() {
-        var randomSaxon = Math.floor(Math.random()* this.saxonArmy.length);
-        var randomViking = Math.floor(Math.random()*this.vikingArmy.length);
-        let attackResult = this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[randomSaxon].strength);
+    // vikingAttack() {
+    //     var randomSaxon = Math.floor(Math.random()* this.saxonArmy.length);
+    //     var randomViking = Math.floor(Math.random()*this.vikingArmy.length);
+    //     let attackResult = this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength);
         
-        // Keep alive Saxons only
-        this.vikingArmy = this.vikingArmy.filter(function (aliveVikings) {
-            return aliveVikings.health > 0;
+    //     // Keep alive Saxons only
+    //     this.saxonArmy = this.saxonArmy.filter(function (aliveSaxons) {
+    //         return aliveSaxons.health > 0;
+    //     });
+
+    //     return attackResult;
+    // }
+
+    // saxonAttack() {
+    //     var randomSaxon = Math.floor(Math.random()* this.saxonArmy.length);
+    //     var randomViking = Math.floor(Math.random()*this.vikingArmy.length);
+    //     let attackResult = this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[randomSaxon].strength);
+        
+    //     // Keep alive Saxons only
+    //     this.vikingArmy = this.vikingArmy.filter(function (aliveVikings) {
+    //         return aliveVikings.health > 0;
+    //     });
+
+    //     return attackResult;
+    // }
+
+
+    // SUPER BONUS VERSION - Refactorisé
+
+    Attack(attacker, defender) {
+        var randomAttacker = Math.floor(Math.random()* attacker.length);
+        var randomDefender = Math.floor(Math.random()*defender.length);    
+
+        let attackResult = defender[randomDefender].receiveDamage(attacker[randomAttacker].strength);
+        
+        // Keep alive soldiers only
+        defender = defender.filter(function (aliveSoldier) {
+            return aliveSoldier.health > 0;
         });
 
         return attackResult;
     }
     
-    // showStatus() {}
+    showStatus() {
+        if (this.saxonArmy.length === 0) {
+            return `Vikings have won the war of the century!`;
+        } else if (this.vikingArmy.length === 0) {
+            return `Saxons have fought for their lives and survived another day...`;
+        }
+            
+        return `Vikings and Saxons are still in the thick of battle.`;
+    }
 }
 
 // new War() // {vikingArmy: []}
