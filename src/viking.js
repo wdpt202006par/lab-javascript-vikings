a6811bb8c5500e6290e40897c3831bfcf312e4de
marinetomazi <m.tomazi@hotmail.fr>
2020-06-21 00:38:53 +0200

    Initial

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,29 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+       return this.strength
+    }
+
+    receiveDamage(theDamage) {
+        this.health -= theDamage
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength){
+        super(health, strength);
+        this.name = name;
+    }
+
+    
+
+}
 
 // Saxon
 class Saxon {}
